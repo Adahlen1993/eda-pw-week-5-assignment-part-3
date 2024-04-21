163fae354ca0c04b00e12746536196221d329410
Alex <adahl1993@gmail.com>
2024-04-21 16:41:00 -0500

    added function

--- assignment/scripts/3-music-collection.js
+++ assignment/scripts/3-music-collection.js
@@ -1,11 +1,28 @@
 console.log('***** Music Collection *****')
 // Safe Zone -- Write code below this line
 
-myCollection = [];
+const myCollection = [];
 
+function addToCollection(collection, title, artist, yearPublished) {
+  this.collection = collection;
+  this.title = title;
+  this.artist = artist;
+  this.yearPublished = yearPublished;
+  let record = {
+    collection: this.collection,
+    title: this.title,
+    artist: this.artist,
+    yearPublished: this.yearPublished
+  };
+  myCollection.push(record);
+  return myCollection[myCollection.length-1];
 
-
-
+  
+}
+console.log(addToCollection('rap', 'because the internet', 'childish gambino', 2013));
+addToCollection('rap', 'At.Long.Last.ASAP', 'A$AP Rocky', 2015);
+console.log(myCollection);
+addToCollection()
 
 
 
